3dae306716aa54b627a0de5c8c3a13f280342860
GrzegorzMika <g.w.mika@gmail.com>
2024-10-16 15:45:36 +0200

    featue:add tests

--- exporters/metrics/metrics_test.go
+++ exporters/metrics/metrics_test.go
@@ -51,3 +51,75 @@ func TestCPUNumberMetric(t *testing.T) {
 		t.Errorf("Expected %f, got %f", expected, m)
 	}
 }
+
+func TestMemoryUsageMetric(t *testing.T) {
+	m := MemoryUsageMetric(example_response)
+	expected := 1975832576.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestMemoryCachedUsageMetric(t *testing.T) {
+	m := MemoryCachedUsageMetric(example_response)
+	expected := 0.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestMemoryLimitMetric(t *testing.T) {
+	m := MemoryLimitMetric(example_response)
+	expected := 33013448704.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkBytesReceivedMetric(t *testing.T) {
+	m := NetworkBytesReceivedMetric(example_response)
+	expected := 88452.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkBytesSentMetric(t *testing.T) {
+	m := NetworkBytesSentMetric(example_response)
+	expected := 1786.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkPacketsReceivedMetric(t *testing.T) {
+	m := NetworkPacketsReceivedMetric(example_response)
+	expected := 1944.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkPacketsSentMetric(t *testing.T) {
+	m := NetworkPacketsSentMetric(example_response)
+	expected := 25.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkErrorsReceivedMetric(t *testing.T) {
+	m := NetworkErrorsReceivedMetric(example_response)
+	expected := 0.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
+
+func TestNetworkErrorsSentMetric(t *testing.T) {
+	m := NetworkErrorsSentMetric(example_response)
+	expected := 0.
+	if m != expected {
+		t.Errorf("Expected %f, got %f", expected, m)
+	}
+}
